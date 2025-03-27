9d2c6e4709236ce16bb4584fd043c29d083bec72
imnxtamess <davide.morisco.work@gmail.com>
2025-03-27 14:18:09 +0100

    Enhance movie list display with genre filter and improved styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,31 @@ import movieData from "../data/movieData";
 export default function App() {
   return (
     <>
-      <div className="main">
+      <header className="d-flex">
+        <h1>Movie List</h1>
+      </header>
+      <main>
         <div className="container">
-          <div className="row row-cols-3 g-3 mt-5">
+          <div className="filter_container d-flex mt-4">
+            <select name="genreFilter" className="genreFilter">
+              <option value="">Filtra per genere</option>
+              {movieData.map((movie) => (
+                <option value={movie.genre}>{movie.genre}</option>
+              ))}
+            </select>
+          </div>
+          <div className="row row-cols-3 g-3 mt-2">
             {movieData.map((movie, index) => (
               <div key={index} className="col">
                 <div className="card card-body">
-                  <h1>{`Title: ${movie.title}`}</h1>
-                  <h2>{`Genre: ${movie.genre}`}</h2>
+                  <h3>{`TITOLO: ${movie.title}`}</h3>
+                  <h5>{`GENERE: ${movie.genre}`}</h5>
                 </div>
               </div>
             ))}
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
