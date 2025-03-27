d5913cb95bc79a289b6992cb8c0286c4e0788cad
imnxtamess <davide.morisco.work@gmail.com>
2025-03-27 14:00:42 +0100

    Add Bootstrap for styling and implement movie display component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import movieData from "../data/movieData";
+
 export default function App() {
   return (
     <>
-      <h1>Ciao</h1>
+      <div className="main">
+        <div className="container">
+          <div className="row row-cols-3 g-3 mt-5">
+            {movieData.map((movie, index) => (
+              <div key={index} className="col">
+                <div className="card card-body">
+                  <h1>{`Title: ${movie.title}`}</h1>
+                  <h2>{`Genre: ${movie.genre}`}</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/data/movieData.js
@@ -0,0 +1,10 @@
+const movieData = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+export default movieData
